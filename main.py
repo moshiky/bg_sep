9fbdb46804e67228dc7dfc5031c0f18636f49158
Moshe Cohen <moshec40@gmail.com>
2021-01-30 19:25:07 +0200

    * Initial commit

---
+++ main.py
@@ -0,0 +1,29 @@
+
+import numpy as np
+import cv2
+
+
+def main():
+    # create capture object
+    cap = cv2.VideoCapture(0)
+
+    while (True):
+        # Capture frame-by-frame
+        ret, frame = cap.read()
+
+        # Our operations on the frame come here
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        # Display the resulting frame
+        cv2.imshow('frame', gray)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    # When everything done, release the capture
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    main()
