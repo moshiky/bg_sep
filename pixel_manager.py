0831c4bd70964754b270cf1d4ec77c2a2445bfc9
Moshe Cohen <moshec40@gmail.com>
2021-01-31 19:17:40 +0200

    * much better background separation - problem in dark areas

--- pixel_manager.py
+++ pixel_manager.py
@@ -1,4 +1,4 @@
-
+import cv2
 import numpy as np
 
 
@@ -6,27 +6,24 @@ class PixelManager:
 
     def __init__(self, height: int, width: int, sample_size: int):
 
-        self._frame_bank = np.zeros((sample_size, int(height), int(width), 3), dtype=np.float32)
+        self._frame_bank = np.zeros((sample_size, int(height), int(width)), dtype=np.float64)
         self._next_idx = 0
 
-        self._centroid_bank = None
+        self._centroid_img = None
         self._max_dist = None
 
     @staticmethod
     def _normalize_frame(frame):
-        cn_min = frame.min(axis=0).min(axis=0)
-        cn_max = frame.max(axis=0).max(axis=0)
-        cn_range = cn_max - cn_min
-
-        return (frame - cn_min) / cn_range
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY).astype(np.float64)
+        return frame
 
     def get_centroid(self):
-        return (self._centroid_bank * np.array([255, 255, 255])).astype(np.uint8)
+        return self._centroid_img.round().astype(np.uint8)
 
     def add_frame(self, frame: np.array):
 
         if self._next_idx < self._frame_bank.shape[0]:
-            self._frame_bank[self._next_idx, :, :, :] = self._normalize_frame(frame)
+            self._frame_bank[self._next_idx, :, :] = self._normalize_frame(frame)
             self._next_idx += 1
 
             return True
@@ -36,22 +33,33 @@ class PixelManager:
 
     @staticmethod
     def _get_dist(im0: np.array, im1: np.array):
-        return np.sqrt(np.power(im0 - im1, 2).sum(axis=-1))
+        return np.sqrt(np.power(im0 - im1, 2))
 
     def calculate_stats(self):
 
-        self._centroid_bank = self._frame_bank.mean(axis=0)
+        centroid_img_base = self._frame_bank.mean(axis=0)
 
-        dist_arr = self._get_dist(self._frame_bank, self._centroid_bank)
+        self._centroid_img = centroid_img_base.copy()
+
+        self._centroid_img += cv2.blur(centroid_img_base, (3, 3))
+        self._centroid_img += cv2.blur(centroid_img_base, (5, 5))
+        self._centroid_img += cv2.blur(centroid_img_base, (9, 9))
+        self._centroid_img += cv2.blur(centroid_img_base, (15, 15))
+        self._centroid_img += cv2.blur(centroid_img_base, (27, 27))
+        self._centroid_img += cv2.blur(centroid_img_base, (41, 41))
+
+        self._centroid_img /= 7
+
+        dist_arr = self._get_dist(self._frame_bank, self._centroid_img)
         self._max_dist = dist_arr.max(axis=0)
 
     def get_mask(self, frame):
 
         # calculate diff for each pixel
-        dist_arr = self._get_dist(self._normalize_frame(frame), self._centroid_bank)
+        norm_frame = self._normalize_frame(frame)
+        dist_arr = self._get_dist(norm_frame, self._centroid_img)
 
         # convert to color mask
-        output_mask = np.zeros_like(frame)
-        output_mask[:, :, 2] = np.round(255 * (dist_arr > self._max_dist * 4).astype(int)).astype(frame.dtype)
+        output_mask = np.round(255 * (dist_arr > self._max_dist * 4)).astype(frame.dtype)
 
         return output_mask
